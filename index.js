f5703691149f24a64933a6086d4950b89c6c1886
William Herman <williamajherman@gmail.com>
2020-07-16 12:01:16 -0400

    started stretch

--- index.js
+++ index.js
@@ -110,9 +110,13 @@ Hint: You can use .splice() for this
 */
 
 function removeFlavorByName(arr, str){
-
-    /*code here*/
-
+    newArray = [...arr];
+    for (let i = 0; i < arr.length; i++){
+        if (newArray[i] === str){
+            newArray.splice(i);
+        }
+    }
+    return newArray;
 }
 
 
@@ -124,10 +128,9 @@ Your function should accept:
 
 and should return a new array that is identical to the old array. You can name the new array however you'd like. */
 
-function copy(/*code here*/){
-
-    /*code here*/
-
+function copy(arrOld, arrNew){
+    arrNew = [...arrOld];
+    return arrNew;
 }
 
 /* Task 7: July 7th is "World Chocolate Day" and Baskin Robins wants to create promotional materials highlighting all of their chocolate flavors. Write a function that checks every item in the array for a given string and returns a new array called filteredArray with just these values. Rather than hardcoding "chocolate" into your function, pass a string as a parameter, and invoke with the argument "chocolate". This way you could also filter for "Vanilla", "Sherbert", etc. when those holidays roll around.
@@ -145,10 +148,14 @@ DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem.
 
 hint - you can use the .includes method to help you solve this */
 
-function filterByWord(/*code here*/){
-
-    /*code here*/
-
+function filterByWord(arr, str){
+   const filteredArray = [];
+   for (i=0; i<arr.length; i++){
+       if (arr[i].includes(str)){
+           filteredArray.push(arr[i]);
+       }
+   }
+   return filteredArray;
 }
 
 
@@ -165,10 +172,9 @@ and should return the average number of words per item in the array.
 
 For example, getAverageWordLength(originalFlavors) should return a number between 0 and 3. */
 
-function getAverageWordLength(/*code here*/){
-
-    /*code here*/
-
+function getAverageWordLength(arr){
+    let wordCount = 0;
+    for (i=0; i<arr.length; 
 }
 
 
